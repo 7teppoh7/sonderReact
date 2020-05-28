6d9a437840ce9c0d7529406d02568a8c619e5c76
7teppoh7 <you@example.com>
2020-05-28 19:48:24 +0400

    Updates

---
+++ precache-manifest.c2540c014d67d04d106629bf2c235d0f.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0698e0fc7466a28323cbaef7f62c9285",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "17a57badf67877b41cc4",
+    "url": "/todolist/static/css/main.768f1f2d.chunk.css"
+  },
+  {
+    "revision": "c10a64383a10268e6700",
+    "url": "/todolist/static/js/2.0db7f94f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/todolist/static/js/2.0db7f94f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17a57badf67877b41cc4",
+    "url": "/todolist/static/js/main.e89014c9.chunk.js"
+  },
+  {
+    "revision": "8132b407ef3c02db4523",
+    "url": "/todolist/static/js/runtime-main.a5263a6d.js"
+  },
+  {
+    "revision": "d863a0969413abc0abbf2e4f08ba0517",
+    "url": "/todolist/static/media/1.d863a096.jpg"
+  },
+  {
+    "revision": "d34fd96946e9d342c30f3aa1c04f9add",
+    "url": "/todolist/static/media/11.d34fd969.jpg"
+  },
+  {
+    "revision": "b703900c4228007858700d2912044aaa",
+    "url": "/todolist/static/media/2.b703900c.jpg"
+  },
+  {
+    "revision": "92186844995645888cb6a8e38ef809e9",
+    "url": "/todolist/static/media/3.92186844.jpg"
+  },
+  {
+    "revision": "ceb15d67d31a9aae65adcd74cd07589f",
+    "url": "/todolist/static/media/4.ceb15d67.jpg"
+  },
+  {
+    "revision": "f5e57f673c0105eff9e967b4cbd1ba84",
+    "url": "/todolist/static/media/5.f5e57f67.jpg"
+  },
+  {
+    "revision": "91ea1549fc527cc75fead60177b967da",
+    "url": "/todolist/static/media/6.91ea1549.jpg"
+  },
+  {
+    "revision": "b36023914d75483261deb64cbceac9e2",
+    "url": "/todolist/static/media/7.b3602391.jpg"
+  },
+  {
+    "revision": "ebbe33f9a67847550bd80d9190df1937",
+    "url": "/todolist/static/media/9.ebbe33f9.jpg"
+  },
+  {
+    "revision": "9e7c186386d94d712b5f6601ce54553e",
+    "url": "/todolist/static/media/forfaq.9e7c1863.jpg"
+  },
+  {
+    "revision": "f773f08b7322c2d0a9c42bd15915ac93",
+    "url": "/todolist/static/media/fornews4.f773f08b.png"
+  },
+  {
+    "revision": "24b6b5759b8a9d60836432260e05dd9f",
+    "url": "/todolist/static/media/insta.24b6b575.png"
+  },
+  {
+    "revision": "2fc4849a676dd089acfe628f2bac8ae1",
+    "url": "/todolist/static/media/logo.2fc4849a.png"
+  },
+  {
+    "revision": "9b8557001e1cc1bbe152ddf01af4383f",
+    "url": "/todolist/static/media/logo2.9b855700.png"
+  },
+  {
+    "revision": "2340428d74e78970b5e57f5f6f577cd3",
+    "url": "/todolist/static/media/photo.2340428d.jpg"
+  },
+  {
+    "revision": "4bfc43188be98deefaf7e30148bf3b00",
+    "url": "/todolist/static/media/photo2.4bfc4318.jpg"
+  },
+  {
+    "revision": "608b763e4d11beeaebb765f5ce9776aa",
+    "url": "/todolist/static/media/vk.608b763e.png"
+  }
+]);
